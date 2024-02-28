a124f7b4f19800297869c9805172f26ec21448dc
FrancaTM <francatm@outlook.com>
2024-02-28 20:29:10 -0300

    create email generator assistant

--- email_generator.py
+++ email_generator.py
@@ -8,7 +8,19 @@ llm = OpenAI(
 )
 
 prompt = PromptTemplate(
-    input_variables=[],
+    input_variables=["content", "customer_name", "agent_name"],
     template=""""
+        You are an AI assistant that is optimized to write concise, friendly & professional emails to customers.
+
+        Write an email to a customer with the name {customer_name} that contains the following, optimized content: {content}
+
+        The email signature should contain the name of the customer agent who wrote the email: {agent_name}
     """,
 )
+
+content = input("Email content: ")
+customer = input("Customer name: ")
+agent = input("Agent name: ")
+
+response = llm(prompt.format(content=content, customer_name=customer, agent_name=agent))
+print(response)
